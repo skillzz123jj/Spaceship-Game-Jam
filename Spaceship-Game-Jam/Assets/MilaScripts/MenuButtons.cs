ca3a22f832efbf47690ee01aabae49218b83fdbb
Mila <turpeinenmila6@gmail.com>
2025-08-30 16:05:30 +0300

    Pause Menu

--- Spaceship-Game-Jam/Assets/MilaScripts/MenuButtons.cs
+++ Spaceship-Game-Jam/Assets/MilaScripts/MenuButtons.cs
@@ -1,18 +1,53 @@
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class MenuButtons : MonoBehaviour
 {
+    bool pauseMenuisOn;
+    [SerializeField] GameObject pauseMenu;
     public void ChangeScene(int scene)
     {
         SceneManager.LoadScene(scene);
-        Debug.Log("New scene!");
     }
 
     public void QuitGame()
     {
         Application.Quit();
-        Debug.Log("Quit game");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseMenuisOn)
+        {
+            ContinueGame();
+
+        }else if (Input.GetKeyDown(KeyCode.Escape) && !pauseMenuisOn)
+        {
+          PauseGame();
+
+        }
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenuisOn = false;
+        pauseMenu.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenuisOn = true;
+        pauseMenu.SetActive(true);
+    }
+
+    public void BackToMenu(int scene)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(scene);
+
     }
 }
