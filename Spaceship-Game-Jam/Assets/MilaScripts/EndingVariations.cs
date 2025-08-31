4fa8008a9446e8319271aea55fdf6abb44ac9258
Mila <turpeinenmila6@gmail.com>
2025-08-31 12:33:45 +0300

    Added materials to the prefabs

--- Spaceship-Game-Jam/Assets/MilaScripts/EndingVariations.cs
+++ Spaceship-Game-Jam/Assets/MilaScripts/EndingVariations.cs
@@ -9,11 +9,22 @@ public class EndingVariations : MonoBehaviour
         { "Jerry Can", "Esa was unable to refuel the rocket and didn't get off the surface of moon."},
         { "Toilet paper ", "Esa had nothing to wipe with and improvised by opening the airlock."},
         { "GPS device", "Esa didn't know the way back to earth and was left out forever in space." },
-        { "Rubberduck", "An asteroid came and made a hole into the rocket, esa had nothing to block it with." },
+        { "Ducky", "An asteroid came and made a hole into the rocket, esa had nothing to block it with." },
         { "Rocket manuals", "Like the ÍKEA manuals we all need, esa had no idea how to assemble the rocket without his manuals." },
         { "Coffee mug", "Without his trusted coffee mug, Esa fell asleep mid-flight and the rocket drifted endlessly through space." },
-        { "Nokia 3310", "Esa forgot his indestructible Nokia 3310 — without it, he had no way to call Earth, and even worse, no Snake to pass the time." }
-
+        { "Nokia 3310", "Esa forgot his indestructible Nokia 3310 — without it, he had no way to call Earth, and even worse, no Snake to pass the time." },
+        { "Antenna", "Without the antenna, Esa’s distress call never got out. He died waving at empty space." },
+        { "Disco ball", "No disco ball meant no party. Without it, morale flatlined — just like Esa." },
+        { "Ducktape", "A crack split the hull. Without duct tape, Esa learned how quickly lungs collapse in space." },
+        { "FloppyDisk", "The launch codes were on a floppy. Esa brought a USB stick instead." },
+        { "Hammer", "One bolt rattled loose. Without a hammer, the ship unraveled piece by piece… with him inside." },
+        { "HDD", "The star maps were on his hard drive. Without it, Esa took the longest scenic route into nothingness." },
+        { "JoyStick", "Esa forgot his joystick — the rocket spun wildly, and he became the first human to orbit endlessly without a single pause." },
+        { "Oxytank", "Forgot O2 tank. Tried holding breath. High score: 27 seconds." },
+        { "SatDish", "No satellite dish = no signal. Esa died with 0 bars and unlimited roaming."},
+        { "Toilet Seat", "The toilet seat was missing. Esa fell into the void mid-relief, leaving only a cosmic stain behind." },
+        { "Toolcase", "No tools meant no fixing the engine. Esa gave it a good kick instead — and the rocket kicked back." }
+        
     };
    
     [SerializeField] TMP_Text endingText;
